72375a57b0c22f0947bfe221ad4090383114b8ff
Amit Kumar <aka883207@gmail.com>
2023-11-07 14:17:35 +0600

    delete and put method created

--- index.js
+++ index.js
@@ -30,12 +30,66 @@ async function run() {
         const result=await cursor.toArray();
         res.send(result)
       })
-      app.get('/all-jobs/:id', async(req,res)=>{
+    app.post('/all-jobs', async(req,res)=>{
+        const job=req.body;
+        const result=await myDB.insertOne(job);
+        res.send(result);
+    })
+    app.get('/all-jobs/:id', async(req,res)=>{
         const id = req.params.id;
+        console.log("id",id);
         const query = {_id: new ObjectId(id)};
+
         const result = await myDB.findOne(query);
         res.send(result)
       })
+    app.get('/update/:id', async(req,res)=>{
+        const id = req.params.id;
+        console.log("id",id);
+        const query = {_id: new ObjectId(id)};
+
+        const result = await myDB.findOne(query);
+        res.send(result)
+      })
+    app.get('/my-jobs/:id', async(req,res)=>{
+        const id = req.params.id;
+        const cursor = myDB.find({
+          userEmail: id,
+        });
+        const result = await cursor.toArray();
+        res.send(result)
+      })
+      app.put('/my-cart/:id', async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: (id)};
+        const options={upsert: true}
+        const updatedJob=req.body;
+        console.log(updatedJob.userEmail,updatedJob.name,updatedJob.photo,updatedJob.brand,updatedJob.price,updatedJob.type,updatedJob.rating,updatedJob.description);
+        const Job={
+          // name,photo,brand,price,type,rating,description
+          $set:{
+            Job_Title: updatedJob.Job_Title,
+            Name: updatedJob.Name,
+            Company_Logo: updatedJob.Company_Logo,
+            Job_Posting_Date: updatedJob.Job_Posting_Date,
+            Salary_Range: updatedJob.Salary_Range,
+            Job_Type: updatedJob.Job_Type,
+            Job_Applicants_Number: updatedJob.Job_Applicants_Number,
+            Application_Deadline: updatedJob.Application_Deadline,
+            Job_Description: updatedJob.Job_Description,
+          }
+          
+        }
+        const resust = await myCart.updateOne(filter,Job,options);
+        res.send(resust)
+      })
+  
+    app.delete('/all-jobs/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await myDB.deleteOne(query);
+        res.send(result);
+      });
     // ==================================================================================
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
